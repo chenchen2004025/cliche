e2a5d1b18b9a339eb50b9ba50ec101438c010469
chenchen <chenchen@gmail.com>
2015-08-28 15:44:29 +0800

    first commit

---
+++ src/main/scala/org/chenchen/cliche/compiler/Main.scala
@@ -0,0 +1,11 @@
+package main.scala.org.chenchen.cliche.compiler
+
+/**
+ * Created by chenc_000 on 2015/8/28.
+ */
+object Main {
+  def main(args: Array[String]) {
+    println("My language")
+  }
+
+}
